f2ed2d10c55c77bcf22dad3cbbc1ed7e49c434e6
Albert Wolchesky <albertwo@microsoft.com>
2018-03-14 02:06:38 -0400

    Updates

---
+++ p20-arm/CLI.sh
@@ -0,0 +1,24 @@
+﻿##SETUP##
+
+
+#Install Azure CLI
+#https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
+
+#Login and set Azure Subscription
+
+az login
+
+az account set --subscription "<SUBSCRIPTION_NAME>" 
+
+
+##DEPLOY TEMPLATE##
+
+
+#https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-template-deploy-cli
+
+az group create --name P20-RG01 --location "eastus2"
+az group deployment create \
+    --name P20Deployment \
+    --resource-group P20-RG01 \
+    --template-file challenge-01-basic-output.json \
+    --parameters genericInput="Hello World"
---
+++ p20-arm/PS.ps1
@@ -0,0 +1,25 @@
+﻿##SETUP##
+
+
+#Install Azure PowerShell 
+#https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.4.1
+
+Install-Module AzureRM -AllowClobber
+
+#Login and set Azure Subscription
+#https://blogs.msdn.microsoft.com/benjaminperkins/2017/08/02/how-to-set-azure-powershell-to-a-specific-azure-subscription/
+
+Login-AzureRmAccount
+
+Set-AzureRmContext -Subscription "<SUBSCRIPTION_NAME>" 
+
+
+##DEPLOY TEMPLATE##
+
+
+#https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-template-deploy
+
+New-AzureRmResourceGroup -Name P20-RG04 -Location eastus2
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName P20-RG04 `
+  -TemplateFile challenge-01-basic-output.json `
+  -genericInput "Hello World"
